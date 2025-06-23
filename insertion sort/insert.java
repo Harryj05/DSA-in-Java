609516107d82c71c0f40b06b06298316efe4c4a2
Harryj05 <harry@Harry.>
2025-06-23 17:01:12 +0530

    insertion sort

--- insertion sort/insert.java
+++ insertion sort/insert.java
@@ -1,37 +1,22 @@
-public class selection {
+public class insert {
   public static void main(String[] args) {
-    int nums[] = {6,5,2,8,9,4};
-    int size = nums.length; 
-    int temp =0;
-    int minIndex = -1;
-    System.out.println("before sorting");
-    for (int num : nums) {
-      System.out.print(num + " ");
-    }
-  
-    for (int i = 0 ; i<size-1; i++){
-      minIndex=i;
-      for (int j = 0; j < size; j++) {
-        if(nums[minIndex] > nums[j])
-          minIndex=j;
-      }
-      temp = nums[minIndex];
-      nums[minIndex]=nums[i];
-      nums[i]=temp;
+    int arr[] = {5,6,2,3,1};
+    for(int i=1; i<arr.length; i++){
+      int key = arr[i];
+      int j = i-1;
 
-      System.out.println();
-      for (int num : nums) {
-        System.out.print(num + " ");
+      while (j>=0 && arr[j] > key)
+      {
+        arr[j+1] = arr[j];
+        j--;
       }
+      arr[j+1] = key;
+    }
+
+    for (int num : arr) {
+      System.out.print(num +" ");
+    }
 
 
-    }
-  
-  
-    System.out.println();
-    System.out.println("after sorting");
-    for (int num : nums) {
-      System.out.print(num + " ");
-      }
-    }
-  } 
+  }
+}
