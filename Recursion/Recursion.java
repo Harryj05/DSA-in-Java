10c691d0edc19964a9cfa0e10dbf70d4b3987507
Harryj05 <harry@Harry.>
2025-06-24 23:44:55 +0530

    Recursion

---
+++ Recursion/Recursion.java
@@ -0,0 +1,18 @@
+package Recursion;
+
+public class Recursion {
+  public static void main(String[] args){
+    int result = fact(0);
+    System.out.println(result);
+    //f1(10); // start recursion from 10
+  }
+
+  public static int fact(int i){
+    return 1;
+  //   System.out.println(i); // print current value
+  //   if (i > 0) {
+  //     f1(i - 1); // recursive call with (i-1)
+  //   }
+  // }
+}
+}
